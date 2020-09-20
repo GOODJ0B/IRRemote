a93b7cfbcad22d0844e6bc072616e8eae977f86e
Job <job.van.der.meiden@hva.nl>
2020-09-20 15:52:20 +0200

    fix

--- infrared/receiver.py
+++ infrared/receiver.py
@@ -3,19 +3,49 @@
 # GPIO for the IR transmitter: 22
 from ircodec.command import CommandSet
 
-from infrared.commands import commands
-
 controller = CommandSet(emitter_gpio=22, receiver_gpio=23, description='TV remote')
+
+power = 'power'
+volume_up = 'volume_up'
+volume_down = 'volume_down'
+up = 'up'
+down = 'down'
+left = 'left'
+right = 'right'
+ok = 'ok'
+back = 'back'
+home = 'home'
+play = 'play'
+pause = 'pause'
+stop = 'stop'
+playpause = 'playpause'
+
+commands = [
+    power,
+    volume_up,
+    volume_down,
+    up,
+    down,
+    left,
+    right,
+    ok,
+    back,
+    home,
+    play,
+    pause,
+    stop,
+    playpause,
+]
 
 # Add the volume up key
 for command in commands:
-    controller.add(command.name)
+    controller.add(command)
 # Connected to pigpio
 # Detecting IR command...
 # Received.
 
 # Remove the volume up command
-controller.remove('volume_up')
+# controller.remove('volume_up')
 
 # Examine the contents of the CommandSet
 controller
