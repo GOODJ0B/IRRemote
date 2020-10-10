cf4dcd5722fceec85fffb62ea789219ea7b2d928
Job <job.van.der.meiden@hva.nl>
2020-10-10 11:54:19 +0200

    finished controller

--- api.py
+++ api.py
@@ -9,22 +9,45 @@ controller = CommandSet.load('samsung-tv.json')
 
 @app.route('/', methods=['GET'])
 def home():
-    return 'Online.\r\nGo to /send-command/command or /receive-command/command.'
+    return 'Online.\r\nGo to /send/ /update/ /add/ /remove/ or /get/'
 
 
-@app.route('/send-command/<command>', methods=['GET'])
+@app.route('/send/<command>', methods=['GET'])
 def sendCommand(command):
     print('=> sending command: ' + command)
     controller.emit(command)
     return 'command send';
 
 
-@app.route('/receive-command/<command>', methods=['GET'])
-def receiveCommand(command):
-    print('=> receiving command: ' + command)
+@app.route('/update/<command>', methods=['GET'])
+def updateCommand(command):
+    print('=> updating command: ' + command)
+    controller.remove(command)
     controller.add(command)
     controller.save_as('samsung-tv.json')
-    return 'command received';
+    return controller.to_json()
+
+
+@app.route('/add/<command>', methods=['GET'])
+def addCommand(command):
+    print('=> adding command: ' + command)
+    controller.add(command)
+    controller.save_as('samsung-tv.json')
+    return controller.to_json()
+
+
+@app.route('/remove/<command>', methods=['GET'])
+def removeCommand(command):
+    print('=> removing command: ' + command)
+    controller.remove(command)
+    controller.save_as('samsung-tv.json')
+    return controller.to_json()
+
+
+@app.route('/get', methods=['GET'])
+def getController():
+    print('=> sending controller')
+    return controller.to_json()
 
 
 app.run(host='0.0.0.0')
